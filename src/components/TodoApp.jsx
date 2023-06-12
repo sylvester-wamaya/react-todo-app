b0abe51902baa349193eb738820971e840831f66
Sylvester Wamaya <sylvesterwamaya@gmail.com>
2023-06-12 22:16:53 +0300

    Add TodoApp component

---
+++ src/components/TodoApp.jsx
@@ -0,0 +1,18 @@
+
+import '@/App.css'
+
+
+function App() {
+
+
+  return (
+    <>
+    <div>
+      <h1>Hello world!</h1>
+      <p>I am in a React Component!</p>
+    </div>
+    </>
+  )
+}
+
+export default App
