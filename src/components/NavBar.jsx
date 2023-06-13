0c47237bac54ae656ddb9ac6b6e596084064c3f3
Sylvester Wamaya <sylvesterwamaya@gmail.com>
2023-06-13 20:38:53 +0300

    Add dropdown events to the navigation bar

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,14 +1,30 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect, useRef } from 'react'
 
 
 function NavBar() {
     const [dropdown, setDropdown] = useState(false);
+    const ref = useRef();
+    console.log(ref)
+
+    useEffect(() => {
+        const handler = (event) => {
+          if (dropdown && ref.current && !ref.current.contains(event.target)) {
+            setDropdown(false);
+          }
+        };
+        document.addEventListener("mousedown", handler);
+        return () => {
+            // Cleanup the event listener
+            document.removeEventListener("mousedown", handler);
+          };
+      }, [dropdown]);
+
   return (
     <nav>
     <ul>
       <li>Home</li>
       <li>About</li>
-      <li>
+      <li ref={ref}>
         <button onClick={()=>setDropdown((prev)=> !prev)}>
           Services <span>&#8595;</span>
         </button>
