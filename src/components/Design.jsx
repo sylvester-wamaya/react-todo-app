d96e6d4ce2565d8a92bf3347132c645057fd1360
Sylvester Wamaya <sylvesterwamaya@gmail.com>
2023-06-17 21:59:19 +0300

    Add Design page

---
+++ src/components/Design.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Header from './Header'
+
+function Design() {
+  return (
+    <div>
+        <Header/>
+      This is design page.
+    </div>
+  )
+}
+
+export default Design
